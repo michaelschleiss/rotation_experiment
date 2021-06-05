1adbf0d1fbcb02e7304de9c48c3a3823778c493b
michaelschleiss <michaelschleiss@gmx.de>
2021-06-05 12:48:39 +0200

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -4,7 +4,7 @@ from torch import nn
 import netvlad
 from torchvision import models
 
-def equiv_net(state_dict=''):
+def equiv_net():
     from backbone import ReResNet
     CHECKPOINT_PATH = 'https://github.com/michaelschleiss/rotation_experiment/releases/download/1/equiv_flip_v_h_best_3_epochs.pth.tar'
     #CHECKPOINT = torch.hub.load_state_dict_from_url(CHECKPOINT_PATH)['state_dict']
