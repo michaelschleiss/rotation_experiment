7f30e122c12a7ec30e614df7eeb85f41eaea6c4e
michaelschleiss <michaelschleiss@gmx.de>
2021-06-05 12:52:01 +0200

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -4,6 +4,18 @@ from torch import nn
 import netvlad
 from torchvision import models
 
+from torchvision.models.resnet import resnet18 as _resnet18
+
+# resnet18 is the name of entrypoint
+def resnet18(pretrained=False, **kwargs):
+    """ # This docstring shows up in hub.help()
+    Resnet18 model
+    pretrained (bool): kwargs, load pretrained weights into the model
+    """
+    # Call the model, load pretrained weights
+    model = _resnet18(pretrained=pretrained, **kwargs)
+    return model
+
 def equiv_net():
     from backbone import ReResNet
     CHECKPOINT_PATH = 'https://github.com/michaelschleiss/rotation_experiment/releases/download/1/equiv_flip_v_h_best_3_epochs.pth.tar'
