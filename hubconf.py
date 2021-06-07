e96c12ccb4150f452d80fe7645b7fb83aa778c94
michaelschleiss <michaelschleiss@gmx.de>
2021-06-07 11:23:17 +0200

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -6,7 +6,8 @@ from torchvision import models
 
 def equiv_net():
     from backbone import ReResNet
-    CHECKPOINT_PATH = 'https://github.com/michaelschleiss/rotation_experiment/releases/download/1/equiv_flip_v_h_best_3_epochs.pth.tar'
+    #CHECKPOINT_PATH = 'https://github.com/michaelschleiss/rotation_experiment/releases/download/1/equiv_flip_v_h_best_3_epochs.pth.tar'
+    CHECKPOINT_PATH = 'https://github.com/michaelschleiss/rotation_experiment/releases/download/1/re_resnet50_c8_batch256-25b16846.pth'
     CHECKPOINT = torch.hub.load_state_dict_from_url(CHECKPOINT_PATH)['state_dict']
 
     model = ReResNet(depth=50)
